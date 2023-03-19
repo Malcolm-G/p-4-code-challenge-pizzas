616329997b639829e3cbdc4340ca9356db29006d
Malcolm-G <malcolm.githuka@gmail.com>
2023-03-19 19:40:08 +0300

    reset db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,12 @@ puts "Seeding..."
         ingredients << Faker::Dessert.topping
     end
     Pizza.create(name:Faker::Food.dish, ingredients:ingredients.join(','))
+end
 
-    random_pizzas = (0..4).to_a.shuffle
+5.times do |i|
+    random_pizzas = (1..5).to_a.shuffle
     3.times do |j|
-        RestaurantPizza.create(price:Faker::Number.between(from: 1, to: 30), pizza_id:random_pizzas[j], restaurant_id:restaurant.id)
+        RestaurantPizza.create(price:Faker::Number.between(from: 1, to: 30), pizza_id:random_pizzas[j], restaurant_id:i+1)
     end
 end
 
