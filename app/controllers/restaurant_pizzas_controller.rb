3177932e67502430e20bbcb2312139bad81425d5
Malcolm-G <malcolm.githuka@gmail.com>
2023-03-19 17:30:48 +0300

    added create action and helper method

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,2 +1,14 @@
 class RestaurantPizzasController < ApplicationController
+    wrap_parameters format: []
+
+    def create
+        restaurant_pizza = RestaurantPizza.create(restaurant_pizza_params)
+        render json: restaurant_pizza
+    end
+
+    private
+
+    def restaurant_pizza_params
+        params.permit(:price,:pizza_id,:restaurant_id)
+    end
 end
