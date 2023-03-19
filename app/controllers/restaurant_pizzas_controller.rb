f24b18eb7cfd12953013485366dc1d7de30fa24e
Malcolm-G <malcolm.githuka@gmail.com>
2023-03-19 17:33:53 +0300

    added rescue response for invalid data

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,4 +1,5 @@
 class RestaurantPizzasController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
     wrap_parameters format: []
 
     def create
@@ -11,4 +12,9 @@ class RestaurantPizzasController < ApplicationController
     def restaurant_pizza_params
         params.permit(:price,:pizza_id,:restaurant_id)
     end
+
+    def render_unprocessable_entity_response(invalid)
+        render json:{errors:invalid.record.errors}, status: :unprocessable_entity
+    end
+    
 end
