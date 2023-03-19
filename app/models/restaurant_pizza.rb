470f959f164bc58af1a9e984e8043a685d58655d
Malcolm-G <malcolm.githuka@gmail.com>
2023-03-19 19:57:24 +0300

    adds validation error meesage

--- app/models/restaurant_pizza.rb
+++ app/models/restaurant_pizza.rb
@@ -2,5 +2,5 @@ class RestaurantPizza < ApplicationRecord
     belongs_to :pizza
     belongs_to :restaurant
     
-    validates :price, inclusion: 1..30
+    validates :price, inclusion: {in:1..30, message:"Price must be between 1 - 30"}
 end
