cceafab67351cf9a85dce3ba37bb611dc92cdb77
Malcolm-G <malcolm.githuka@gmail.com>
2023-03-19 15:56:12 +0300

    refactored seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,16 @@
 puts "Seeding..."
 5.times do
     restaurant = Restaurant.create(name:Faker::Restaurant.name, address:Faker::Address.full_address)
-
-    random_pizzas = (0..4).to_a.shuffle
-    3.times do |j|
-        RestaurantPizza.create(price: Faker::Number.between(from: 1, to: 30), pizza_id:random_pizzas[j], restaurant:restaurant)
-    end
-end
-
-5.times do
     ingredients = []
     5.times do
         ingredients << Faker::Dessert.topping
     end
-    Pizza.create(name:Faker::Food.dish, ingredients:ingredients)
+    Pizza.create(name:Faker::Food.dish, ingredients:ingredients.join(','))
+
+    random_pizzas = (0..4).to_a.shuffle
+    3.times do |j|
+        RestaurantPizza.create(price:Faker::Number.between(from: 1, to: 30), pizza_id:random_pizzas[j], restaurant_id:restaurant.id)
+    end
 end
 
 puts "Done Seeding!"
